261c2ed32526ffc40651ad2275bf7cced8a404f8
yann <ydurand@4cad.fr>
2018-10-31 10:23:44 +0100

    Gros commit de set-up

---
+++ src/Controller/EnumAction.java
@@ -0,0 +1,6 @@
+package Controller;
+
+public enum EnumAction {
+    LOGIN_REMOTE_SERVER,
+    LOGIN_LOCAL_SERVER
+}
---
+++ src/View/viewLocalLoginFrame.java
@@ -0,0 +1,8 @@
+package View;
+
+import javax.swing.*;
+
+public class viewLocalLoginFrame {
+    private JPanel MainJPanel;
+    private JLabel JLabelLogin;
+}
